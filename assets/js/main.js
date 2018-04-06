c035a400e6920036c8e1e01f34876c7adbad40a3
Fernando Moraes <fermoraeslima10@gmail.com>
2018-04-06 11:20:40 -0300

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 function initMap() {
 
   var positionEvent = {
-    lat: -23.295926,
-    lng: -47.680285
+    lat: -23.288552,
+    lng: -47.651122
   };
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 9,
+    zoom: 12,
     center: positionEvent,
     scrollwheel: false,
     streetViewControl: false
